a1d791d5676a7bbece9cd16d81a3cd9be8ed21bc
davidndrum <108105462+davidndrum@users.noreply.github.com>
2022-12-15 23:37:46 -0500

    This attempt to make the GUI work with the data handling modules and class has failed.

--- SDEV140_FP_customer.py
+++ SDEV140_FP_customer.py
@@ -9,16 +9,16 @@ This module declares a class to define a customer's data.
 class Customer:
     """Class that sets the expected values for a customer record."""
 
-    def __init__(self):
-        self.first = input("What is the customer's first name?")
-        self.last = input("What is the customer's last name?")
-        self.streetNumber = input("What is the customer's street number?")
-        self.streetName = "  " + input("What street does the customer live on?")
-        self.city = input("What city does the customer live in?")
-        self.stateID = input("What state does the customer live in?  Enter the 2 character postal code (IN:Indiana) ?")
-        self.zipCode = input("What is the customer's zipcode?")
-        self.timePreference = int(input("What time does the customer prefer service? 1:Morning, 2:Afternoon, 3:Evening"))
-        self.weekdayPreference = input("Enter a day of the week for service. S, M, T, W, TH, F, Sa")
+    def __init__(self, first, last, streetNumber, streetName, city, stateID, zipCode, timePreference, weekdayPreference):
+        self.first = first
+        self.last = last
+        self.streetNumber = streetNumber
+        self.streetName = "  " + streetName
+        self.city = city
+        self.stateID = stateID
+        self.zipCode = zipCode
+        self.timePreference = int(timePreference)
+        self.weekdayPreference = weekdayPreference
 
     def __str__(self):
         """This creates a function that puts the entire record in a string."""
